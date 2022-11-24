82ee69da972478f16002526bcc57ebfc6ffbf248
hongjiang <hongjiang123456>
2022-11-24 17:33:07 +0800

    打开目标页面 页面滚动

---
+++ examples/src/main/java/com/teamdev/jxbrowser/examples/seo/ua/OSVersion.java
@@ -0,0 +1,94 @@
+/*
+ *  Copyright 2022, TeamDev. All rights reserved.
+ *
+ *  Redistribution and use in source and/or binary forms, with or without
+ *  modification, must retain the above copyright notice and the following
+ *  disclaimer.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.teamdev.jxbrowser.examples.seo.ua;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class OSVersion {
+    public List<String> macOsVersion = Arrays.asList(
+            "Macintosh; Intel Mac OS X 10_15_7",
+            "Macintosh; Intel Mac OS X 10_15_6",
+            "Macintosh; Intel Mac OS X 10_15_5",
+            "Macintosh; Intel Mac OS X 10_15_4",
+            "Macintosh; Intel Mac OS X 10_15_3",
+            "Macintosh; Intel Mac OS X 10_15_2",
+            "Macintosh; Intel Mac OS X 10_15_1",
+            "Macintosh; Intel Mac OS X 10_15_0",
+            "Macintosh; Intel Mac OS X 10_14_6",
+            "Macintosh; Intel Mac OS X 10_14_5",
+            "Macintosh; Intel Mac OS X 10_14_4",
+            "Macintosh; Intel Mac OS X 10_14_3",
+            "Macintosh; Intel Mac OS X 10_14_2",
+            "Macintosh; Intel Mac OS X 10_14_1",
+            "Macintosh; Intel Mac OS X 10_14_0",
+            "Macintosh; Intel Mac OS X 10_13_6",
+            "Macintosh; Intel Mac OS X 10_13_5",
+            "Macintosh; Intel Mac OS X 10_13_4",
+            "Macintosh; Intel Mac OS X 10_13_3",
+            "Macintosh; Intel Mac OS X 10_13_2",
+            "Macintosh; Intel Mac OS X 10_13_1",
+            "Macintosh; Intel Mac OS X 10_13_0",
+            "Macintosh; Intel Mac OS X 10_12_6",
+            "Macintosh; Intel Mac OS X 10_12_5",
+            "Macintosh; Intel Mac OS X 10_12_4",
+            "Macintosh; Intel Mac OS X 10_12_3",
+            "Macintosh; Intel Mac OS X 10_12_2",
+            "Macintosh; Intel Mac OS X 10_12_1",
+            "Macintosh; Intel Mac OS X 10_12_0",
+            "Macintosh; Intel Mac OS X 10_11_6",
+            "Macintosh; Intel Mac OS X 10_11_5",
+            "Macintosh; Intel Mac OS X 10_11_4",
+            "Macintosh; Intel Mac OS X 10_11_3",
+            "Macintosh; Intel Mac OS X 10_11_2",
+            "Macintosh; Intel Mac OS X 10_11_1",
+            "Macintosh; Intel Mac OS X 10_11_0",
+            "Macintosh; Intel Mac OS X 10_10_5",
+            "Macintosh; Intel Mac OS X 10_10_4",
+            "Macintosh; Intel Mac OS X 10_10_3",
+            "Macintosh; Intel Mac OS X 10_10_2",
+            "Macintosh; Intel Mac OS X 10_10_1",
+            "Macintosh; Intel Mac OS X 10_10_0",
+            "Macintosh; Intel Mac OS X 10_9_5",
+            "Macintosh; Intel Mac OS X 10_9_4",
+            "Macintosh; Intel Mac OS X 10_9_3",
+            "Macintosh; Intel Mac OS X 10_9_2",
+            "Macintosh; Intel Mac OS X 10_9_1",
+            "Macintosh; Intel Mac OS X 10_9_0",
+            "Macintosh; Intel Mac OS X 10_8_5",
+            "Macintosh; Intel Mac OS X 10_8_4",
+            "Macintosh; Intel Mac OS X 10_8_3",
+            "Macintosh; Intel Mac OS X 10_8_2",
+            "Macintosh; Intel Mac OS X 10_8_1",
+            "Macintosh; Intel Mac OS X 10_8_0"
+            );
+
+    public List<String> windowsOsVersion = Arrays.asList(
+            "Windows NT 10.0; Win64; x64",
+            "Windows NT 10.0",
+            "Windows NT 6.3; Win64; x64",
+            "Windows NT 6.3",
+            "Windows NT 6.2; Win64; x64",
+            "Windows NT 6.2",
+            "Windows NT 6.1; Win64; x64",
+            "Windows NT 6.1"
+    );
+}
